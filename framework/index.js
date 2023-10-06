9194bf515475627a7d43c24065ec98666d0a1f3d
Nathan Gaul <nathanjgaul@gmail.com>
2023-10-05 20:35:46 -0500

    Events

--- framework/index.js
+++ framework/index.js
@@ -1,8 +1,13 @@
+import * as snabbdom from "snabbdom";
+
+const patch = snabbdom.init([snabbdom.eventListenersModule]);
+
 export const init = (selector, component) => {
   const app = document.querySelector(selector);
-  const newElement = document.createElement(component.type);
-  const newTextContent = document.createTextNode(component.template);
-
-  newElement.append(newTextContent);
-  app.append(newElement);
+  patch(app, component.template);
 };
+
+export const CreateComponent =
+  ({ template, methods = {}, initialState = {} }) =>
+  (props) =>
+    template(props);
--- src/user.js
+++ src/user.js
@@ -1,4 +1,14 @@
+import { CreateComponent } from "../framework";
 import { div } from "../framework/element";
+import { onClick } from "../framework/event";
 
-export const User = ({ firstName, lastName }) =>
-  div`Hello ${firstName} ${lastName}`;
+const initialState = { firstName: "Nathan", lastName: "Gaul" };
+
+const methods = {
+  changeName: (state, firstName) => ({ ...state, firstName }),
+};
+
+const template = ({ firstName, lastName }) =>
+  div`${onClick(() => alert(firstName))} Hello ${firstName} ${lastName}`;
+
+export const User = CreateComponent({ template, methods, initialState });
